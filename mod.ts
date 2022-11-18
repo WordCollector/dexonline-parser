48ecb379201ec914608d5dd28b0d5e59c9818d3d
vxern <vxern@wordcollector.co.uk>
2022-11-18 23:14:47 +0000

    refactor: Move top-level types to `types.ts`.

--- mod.ts
+++ mod.ts
@@ -1,75 +1,17 @@
 import * as csv from 'std/encoding/csv.ts';
 import { default as convertTableToCSV } from 'table-to-csv';
 import { Cheerio, CheerioAPI, Element, load } from 'cheerio';
-import { ContentTabs, Expressions, Links, Selectors, valueToEnum, zip } from './src/mod.ts';
-
-/** Specifies the strictness of word matching. */
-enum MatchingModes {
-	/** Consider only lemmas that match the search term exactly. */
-	Strict,
-
-	/** Consider all lemmas similar to the search term. */
-	Lax,
-}
-
-/** Defines the available options for getting a word from the dictionary. */
-interface ParserOptions {
-	/**
-	 * Specifies the mode to use by the parser for matching results to the search term.
-	 *
-	 * @defaultValue `SearchModes.Lax`
-	 */
-	mode: MatchingModes;
-
-	/** Configures Dexonline's response. */
-	flags: DictionaryFlags;
-}
-
-/**
- * Bit-based flags for configuring the dictionary and the results sent back by it.
- *
- * @privateRemarks
- * At the time of writing, there are just 5 options.
- * The 5th option ('always show advanced search bar') has no function outside of UI.
- * If for any case there were a need to add support for this option, its bit value is __64__.
- * The preferences with bit values 16, 32 and 128 are no longer used.
- */
-enum DictionaryFlags {
-	/** No flags. */
-	None = 0,
-
-	/**
-	 * Replace letters with the comma diacritic ('ș', 'ț') with their cedilla variants ('ş', 'ţ').
-	 */
-	UseCedillas = 1,
-
-	/**
-	 * Do not include words that are identical except for diacritics.
-	 *
-	 * For example, without this flag enabled, a query for the word 'ca' will also return 'că'.
-	 */
-	MatchDiacritics = 2,
-
-	/**
-	 * Use the orthography from before the 1993 reform. (sînt, cînd, rîu, vîjîi)
-	 */
-	UsePreReformOrthography = 4,
-
-	/**
-	 * Return entries only from normative dictionaries published by the Institute of Linguistics
-	 * (Institutul de Lingvistică) of the Romanian Academy (Academia Română).
-	 *
-	 * Enabling this flag will ensure that the only results provided are from the latest editions
-	 * of the DEX and the DOOM.
-	 *
-	 * @remarks
-	 * DEX - Explanatory dictionary of the Romanian language (Dicționarul explicativ al limbii
-	 * române)
-	 * DOOM - Orthographic, orthoepic and morphological dictionary of the Romanian language
-	 * (Dicționarul ortografic, ortoepic și morfologic al limbii române)
-	 */
-	SearchOnlyNormativeDictionaries = 8,
-}
+import {
+	ContentTabs,
+	DictionaryFlags,
+	Expressions,
+	Links,
+	MatchingModes,
+	ParserOptions,
+	Selectors,
+	valueToEnum,
+	zip,
+} from './src/mod.ts';
 
 /** The default search options. */
 const defaultSearchOptions: ParserOptions = {
